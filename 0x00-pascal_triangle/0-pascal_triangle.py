918e5990aec3ebfb31ec60ffe568dacde474861a
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-28 10:35:43 +0300

    fixing pycodestyle failure

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""return a list of lists of integers representing the Pascal’s triangle of n"""
+"""return a list of lists of integers the Pascal’s triangle of n"""
+
 
 def pascal_triangle(n):
+    """Function take n as value and return pascal triangle"""
     if (n <= 0):
         return []
     elif (n == 1):
         return [[1]]
     elif (n == 2):
-        return [[1],[1, 1]]
-    
-    result = [[1],[1, 1]]
+        return [[1], [1, 1]]
+
+    result = [[1], [1, 1]]
     for i in range(3, n + 1):
         row = [1]
         for j in range(0, i - 1):
