24672e15d8a72d16dec0be569e572f0c822d67e1
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-13 12:56:18 +0300

    add files 0-stats.py, 0-generator.py  and README

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""reads stdin line by line and computes metrics"""
+import sys
+
+
+def log_parser():
+    """Parse log, print stat and file size"""
+    out = {'200': 0,
+           '301': 0,
+           '400': 0,
+           '401': 0,
+           '403': 0,
+           '404': 0,
+           '405': 0,
+           '500': 0}
+    file_size = 0
+    count = 0
+    try:
+        for line in sys.stdin:
+            li = line.split()
+            if (len(li) == 9):
+                if (count == 9):
+                    print(f"File size: {file_size}")
+                    for key in out:
+                        if (out[key]):
+                            print(f"{key}: {out[key]}")
+                    count = 0
+                else:
+                    count += 1
+                out[li[7]] += 1
+                file_size += int(li[8])
+    except KeyboardInterrupt:
+        print(f"File size: {file_size}")
+        for key in out:
+            if (out[key]):
+                print(f"{key}: {out[key]}")
+
+
+if __name__ == "__main__":
+    log_parser()
