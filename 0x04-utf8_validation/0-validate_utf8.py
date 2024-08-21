438b60498e85cdae59f09e4c1b0f854ceb3f4446
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-21 14:51:26 +0300

    fixing file 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,7 +3,7 @@
 
 
 def validUTF8(data):
-    """"determine if the given data set represents a valid UTF-8 encoding"""
+    """determine if the given data set represents a valid UTF-8 encoding"""
     for i in data:
         if (i < 0 or i > 127):
             return False
